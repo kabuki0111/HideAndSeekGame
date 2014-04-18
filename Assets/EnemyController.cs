f3acb649a832d7779be1a7f2a9a52f5f04557300
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-18 18:22:19 +0900

    敵の視点システムの基礎ロジックが出来たためコミット

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -6,22 +6,57 @@ public class EnemyController : MonoBehaviour {
 
 	public	Transform	playerModelGameObjectTrans;
 	public	Transform[] changeMoveEnemyPoint;
+	public	float		fieldOfViewAngle		= 110f;				// Number of degrees, centred on forward, for the enemy see.
+	//public	SphereCollider	col;
 
-	private NavMeshAgent agent;
+	private NavMeshAgent	agent;
+	private bool			isPlayerInSight;
+	private SphereCollider	col;
+	private GameObject		playerGameObject;
 
 	void Start(){
-		agent = GetComponent<NavMeshAgent>();
+		agent				= GetComponent<NavMeshAgent>();
+		col					= GetComponent<SphereCollider>();
+		playerGameObject	= GameObject.FindGameObjectWithTag(DoneTags.player);
 	}
 
 	void Update(){
-		agent.SetDestination(changeMoveEnemyPoint[0].position);
-		//EnemmyMove();
+		if(isPlayerInSight){
+			agent.SetDestination(playerGameObject.transform.position);
+		}else{
+		 	agent.SetDestination(changeMoveEnemyPoint[0].position);
+		}
 	}
 
-	private void EnemmyMove(){
-		Vector3 newRotation	= Quaternion.LookRotation(changeMoveEnemyPoint[0].position - playerModelGameObjectTrans.position).eulerAngles;
-		newRotation.x		= 0;
-		newRotation.z		= 0;
-		playerModelGameObjectTrans.rotation = Quaternion.Slerp(playerModelGameObjectTrans.rotation, Quaternion.Euler(newRotation), Time.deltaTime);
+
+	void OnTriggerStay (Collider other){
+		if(other.gameObject == playerGameObject){
+			Debug.Log("UC");
+			isPlayerInSight = false;
+
+			Vector3 direction	= other.transform.position - transform.position;
+			float	angle		= Vector3.Angle(direction, transform.forward);
+			
+			if(angle < fieldOfViewAngle * 0.5f){
+				RaycastHit hit;
+				var layerMask =  1<<8;
+				if(Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask)){
+					Debug.Log("agagagas ----> "+hit.collider.gameObject.name);
+					if(hit.collider.gameObject == playerGameObject){
+						isPlayerInSight = true;
+						Debug.Log(">>>> "+isPlayerInSight);
+					}
+				}
+			}
+
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+//		if(other.gameObject == playerGameObject){
+//			isPlayerInSight = false;
+//		}
 	}
 }
