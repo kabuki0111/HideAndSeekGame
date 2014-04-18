9f07d53a2d29731b636f9a335b872d324b2079c5
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-18 19:50:41 +0900

    敵AIに巡回アルゴリズムを導入

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -13,50 +13,54 @@ public class EnemyController : MonoBehaviour {
 	private bool			isPlayerInSight;
 	private SphereCollider	col;
 	private GameObject		playerGameObject;
+	private	int				patrolIndex;
 
 	void Start(){
 		agent				= GetComponent<NavMeshAgent>();
 		col					= GetComponent<SphereCollider>();
 		playerGameObject	= GameObject.FindGameObjectWithTag(DoneTags.player);
+		patrolIndex			= 0;
 	}
 
 	void Update(){
 		if(isPlayerInSight){
 			agent.SetDestination(playerGameObject.transform.position);
 		}else{
-		 	agent.SetDestination(changeMoveEnemyPoint[0].position);
+			//agent.SetDestination(changeMoveEnemyPoint[0].position);
+			Patrol();
 		}
 	}
 
+	private void Patrol(){
+		agent.SetDestination(changeMoveEnemyPoint[patrolIndex].position);
+		float patrolPointX	= changeMoveEnemyPoint[patrolIndex].position.x;
+		float patrolPointZ	= changeMoveEnemyPoint[patrolIndex].position.z;
+		if(transform.position.x==patrolPointX && transform.position.z==patrolPointZ){
+			patrolIndex ++;
+			if(patrolIndex >= changeMoveEnemyPoint.Length){
+				patrolIndex = 0;
+			}
+		}
+	}
 
 	void OnTriggerStay (Collider other){
 		if(other.gameObject == playerGameObject){
-			Debug.Log("UC");
-			isPlayerInSight = false;
-
 			Vector3 direction	= other.transform.position - transform.position;
 			float	angle		= Vector3.Angle(direction, transform.forward);
 			
 			if(angle < fieldOfViewAngle * 0.5f){
-				RaycastHit hit;
-				var layerMask =  1<<8;
-				if(Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask)){
-					Debug.Log("agagagas ----> "+hit.collider.gameObject.name);
+				RaycastHit	hit;
+				var			layerMask		= 1<<8;
+				bool		isFindPlayer	= Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask);
+				if(isFindPlayer){
 					if(hit.collider.gameObject == playerGameObject){
 						isPlayerInSight = true;
-						Debug.Log(">>>> "+isPlayerInSight);
 					}
 				}
 			}
-
 		}
 	}
-	
-	
-	void OnTriggerExit (Collider other)
-	{
-//		if(other.gameObject == playerGameObject){
-//			isPlayerInSight = false;
-//		}
+
+	void OnTriggerExit (Collider other){
 	}
 }
