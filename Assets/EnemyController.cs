601aa05d56c3915ec790b50df99ca1bdac02601a
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-18 14:54:12 +0900

    NaviMeshを使い、敵AIを作成。一旦、シンプルに動く状態でコミット

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -3,26 +3,25 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	private const float MOVE_SPEED_ADJUSTMENT = 0.05f;
-	//private const ENMMY_POINT_GAMEOBJECT_PATH = "EnemyAllRootObject";
-	public	Transform	playerModelGameObjectTrans;
-	public Transform[] changeMoveEnemyPoint;
 
+	public	Transform	playerModelGameObjectTrans;
+	public	Transform[] changeMoveEnemyPoint;
+
+	private NavMeshAgent agent;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+	}
 
 	void Update(){
+		agent.SetDestination(changeMoveEnemyPoint[0].position);
+		//EnemmyMove();
+	}
+
+	private void EnemmyMove(){
 		Vector3 newRotation	= Quaternion.LookRotation(changeMoveEnemyPoint[0].position - playerModelGameObjectTrans.position).eulerAngles;
 		newRotation.x		= 0;
 		newRotation.z		= 0;
 		playerModelGameObjectTrans.rotation = Quaternion.Slerp(playerModelGameObjectTrans.rotation, Quaternion.Euler(newRotation), Time.deltaTime);
-
-	}
-
-	private void EnemmyMove(Transform enemmyTrans){
-//		float	axisHorizontalValue	= enemmyTrans.position.x * MOVE_SPEED_ADJUSTMENT;
-//		float	axisVerticalValue	= enemmyTrans.position.y * MOVE_SPEED_ADJUSTMENT;
-//		transform.rotation = Quaternion.LookRotation(enemmyTrans.position);
-//		transform.position += new Vector3(axisHorizontalValue, 0, axisVerticalValue);
-//		var newRotation = Quaternion.LookRotation(enemmyTrans.position - transform.position).eulerAngles;
-//		var angles		= transform.rotation.eulerAngles;
-//		transform.rotation = Quaternion.Euler(angles.x, 0, angles.z);
 	}
 }
