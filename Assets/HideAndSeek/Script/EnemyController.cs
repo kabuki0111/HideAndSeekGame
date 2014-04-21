da012932a80d644d162e9568d305fcd0110f5a59
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-21 14:40:22 +0900

    エネミーにUnityちゃんのモデルを導入（アニメーション対応中）

--- Assets/HideAndSeek/Script/EnemyController.cs
+++ Assets/HideAndSeek/Script/EnemyController.cs
@@ -4,45 +4,58 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	private const float MOVE_SPEED_ADJUSTMENT = 0.05f;
 
-	public	Transform	playerModelGameObjectTrans;
+	public float animSpeed = 1.5f;				// アニメーション再生速度設定
+	public	Transform playerModelGameObjectTrans;
 	public	Transform[] changeMoveEnemyPoint;
-	public	float		fieldOfViewAngle		= 110f;				// Number of degrees, centred on forward, for the enemy see.
+	public	float fieldOfViewAngle		= 110f;				// Number of degrees, centred on forward, for the enemy see.
 	//public	SphereCollider	col;
 
-	private NavMeshAgent	agent;
-	private bool			isPlayerInSight;
-	private SphereCollider	col;
-	private GameObject		playerGameObject;
-	private	int				patrolIndex;
-	private	float			stopChaseTimer;
+	private NavMeshAgent agent;
+	private bool isPlayerInSight;
+	private SphereCollider col;
+	private GameObject playerGameObject;
+	private	int patrolIndex;
+	private	float stopChaseTimer;
+
+	private Animator anim;								// キャラにアタッチされるアニメーターへの参照
+	private AnimatorStateInfo currentBaseState;			// base layerで使われる、アニメーターの現在の状態の参照
 
 	void Start(){
-		agent				= GetComponent<NavMeshAgent>();
-		col					= GetComponent<SphereCollider>();
-		playerGameObject	= GameObject.FindGameObjectWithTag(DoneTags.player);
-		patrolIndex			= 0;
+		// Animatorコンポーネントを取得する
+		anim = GetComponent<Animator>();
+
+		agent = GetComponent<NavMeshAgent>();
+		col = GetComponent<SphereCollider>();
+		playerGameObject = GameObject.FindGameObjectWithTag(DoneTags.player);
+		patrolIndex = 0;
 	}
 
-	void Update(){
+	void FixedUpdate(){
 		if(isPlayerInSight){
 			agent.SetDestination(playerGameObject.transform.position);
 			if(Vector3.Distance(transform.position, playerGameObject.transform.position) > 10f){
 				stopChaseTimer += Time.deltaTime;
 				if(stopChaseTimer > 5f){
 					Debug.Log("stop!!");
-					stopChaseTimer	= 0;
+					stopChaseTimer = 0;
 					isPlayerInSight = false;
 				}
 			}
 		}else{
+			float h = Input.GetAxis("Horizontal");				// 入力デバイスの水平軸をhで定義
+			float v = Input.GetAxis("Vertical");				// 入力デバイスの垂直軸をvで定義
+			anim.SetFloat("Speed", v);							// Animator側で設定している"Speed"パラメタにvを渡す
+			anim.SetFloat("Direction", h); 						// Animator側で設定している"Direction"パラメタにhを渡す
+			anim.speed = animSpeed;								// Animatorのモーション再生速度に animSpeedを設定する
+			currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// 参照用のステート変数にBase Layer (0)の現在のステートを設定する
 			Patrol();
 		}
 	}
 
 	private void Patrol(){
 		agent.SetDestination(changeMoveEnemyPoint[patrolIndex].position);
-		float patrolPointX	= changeMoveEnemyPoint[patrolIndex].position.x;
-		float patrolPointZ	= changeMoveEnemyPoint[patrolIndex].position.z;
+		float patrolPointX = changeMoveEnemyPoint[patrolIndex].position.x;
+		float patrolPointZ = changeMoveEnemyPoint[patrolIndex].position.z;
 		if(transform.position.x == patrolPointX && transform.position.z==patrolPointZ){
 			patrolIndex ++;
 			if(patrolIndex >= changeMoveEnemyPoint.Length){
@@ -53,13 +66,13 @@ public class EnemyController : MonoBehaviour {
 
 	private void  OutLook(Collider other){
 		if(other.gameObject == playerGameObject){
-			Vector3 direction	= other.transform.position - transform.position;
-			float	angle		= Vector3.Angle(direction, transform.forward);
+			Vector3 direction = other.transform.position - transform.position;
+			float	angle = Vector3.Angle(direction, transform.forward);
 			
 			if(angle < fieldOfViewAngle * 0.5f){
-				RaycastHit	hit;
-				var			layerMask		= 1<<10;
-				bool		isFindPlayer	= Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask);
+				RaycastHit hit;
+				var layerMask = 1<<10;
+				bool isFindPlayer = Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask);
 				if(isFindPlayer){
 					if(hit.collider.gameObject == playerGameObject){
 						Debug.Log("hit player!!");
--- Assets/HideAndSeek/Script/PlayerController.cs
+++ Assets/HideAndSeek/Script/PlayerController.cs
@@ -5,11 +5,11 @@ public class PlayerController : MonoBehaviour {
 	private const float MOVE_SPEED_ADJUSTMENT = 0.05f;
 
 	void Update () {
-		float	axisHorizontalValue	= Input.GetAxis("Horizontal")	* MOVE_SPEED_ADJUSTMENT;
-		float	axisVerticalValue	= Input.GetAxis("Vertical")		* MOVE_SPEED_ADJUSTMENT;
-		Vector3 axisTotalVector3	= new Vector3(axisHorizontalValue, 0, axisVerticalValue);
+		float axisHorizontalValue = Input.GetAxis("Horizontal")	* MOVE_SPEED_ADJUSTMENT;
+		float axisVerticalValue = Input.GetAxis("Vertical")		* MOVE_SPEED_ADJUSTMENT;
+		Vector3 axisTotalVector3 = new Vector3(axisHorizontalValue, 0, axisVerticalValue);
 
-		transform.rotation	= Quaternion.LookRotation(axisTotalVector3);
+		transform.rotation = Quaternion.LookRotation(axisTotalVector3);
 		transform.position += axisTotalVector3;
 	}
 }
