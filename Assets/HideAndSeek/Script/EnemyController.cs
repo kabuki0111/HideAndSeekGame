8fd94a48a037fa7a80fe65d45b721f7101b505a2
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-21 17:35:11 +0900

    不具合は残っているがアニメーションを作成

--- Assets/HideAndSeek/Script/EnemyController.cs
+++ Assets/HideAndSeek/Script/EnemyController.cs
@@ -5,35 +5,35 @@ public class EnemyController : MonoBehaviour {
 	private const float MOVE_SPEED_ADJUSTMENT = 0.05f;
 
 	public float animSpeed = 1.5f;				// アニメーション再生速度設定
-	public	Transform playerModelGameObjectTrans;
-	public	Transform[] changeMoveEnemyPoint;
-	public	float fieldOfViewAngle		= 110f;				// Number of degrees, centred on forward, for the enemy see.
-	//public	SphereCollider	col;
+	public Transform playerModelGameObjectTrans;
+	public Transform[] changeMoveEnemyPoint;
+	public float fieldOfViewAngle = 110f;				// Number of degrees, centred on forward, for the enemy see.
+	public float deadZone = 5f;
 
 	private NavMeshAgent agent;
 	private bool isPlayerInSight;
-	private SphereCollider col;
+	private SphereCollider opticSphereCol;
 	private GameObject playerGameObject;
 	private	int patrolIndex;
 	private	float stopChaseTimer;
 
 	private Animator anim;								// キャラにアタッチされるアニメーターへの参照
 	private AnimatorStateInfo currentBaseState;			// base layerで使われる、アニメーターの現在の状態の参照
+	private DoneAnimatorSetup animSetup;				// An instance of the AnimatorSetup helper class.
 
-	void Start(){
-		// Animatorコンポーネントを取得する
+	private DoneHashIDs hash;
+
+	void Awake(){
 		anim = GetComponent<Animator>();
-
 		agent = GetComponent<NavMeshAgent>();
-		col = GetComponent<SphereCollider>();
+		opticSphereCol = GetComponent<SphereCollider>();
 		playerGameObject = GameObject.FindGameObjectWithTag(DoneTags.player);
+		hash = GameObject.FindGameObjectWithTag(DoneTags.gameController).GetComponent<DoneHashIDs>();
+		animSetup = new DoneAnimatorSetup(anim, hash);
 		patrolIndex = 0;
-		anim.SetBool("Idle", false);
 	}
 
-	void FixedUpdate(){
-		//anim.SetBool("Locomotion", true);
-
+	void Update(){
 		if(isPlayerInSight){
 			agent.SetDestination(playerGameObject.transform.position);
 			if(Vector3.Distance(transform.position, playerGameObject.transform.position) > 10f){
@@ -45,15 +45,11 @@ public class EnemyController : MonoBehaviour {
 				}
 			}
 		}else{
-			float h = Input.GetAxis("Horizontal");				// 入力デバイスの水平軸をhで定義
-			float v = Input.GetAxis("Vertical");				// 入力デバイスの垂直軸をvで定義
-			anim.SetFloat("Speed", v);							// Animator側で設定している"Speed"パラメタにvを渡す
-			anim.SetFloat("Direction", h); 						// Animator側で設定している"Direction"パラメタにhを渡す
-			anim.speed = animSpeed;								// Animatorのモーション再生速度に animSpeedを設定する
-			currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// 参照用のステート変数にBase Layer (0)の現在のステートを設定する
-			anim.SetBool("Locomotion", true);
 			Patrol();
 		}
+
+		NavAnimSetup();
+//		animSetup.Setup(speed, angle);
 	}
 
 	private void Patrol(){
@@ -76,7 +72,7 @@ public class EnemyController : MonoBehaviour {
 			if(angle < fieldOfViewAngle * 0.5f){
 				RaycastHit hit;
 				var layerMask = 1<<10;
-				bool isFindPlayer = Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, col.radius, layerMask);
+				bool isFindPlayer = Physics.Raycast(transform.position+transform.up, direction.normalized, out hit, opticSphereCol.radius, layerMask);
 				if(isFindPlayer){
 					if(hit.collider.gameObject == playerGameObject){
 						Debug.Log("hit player!!");
@@ -85,6 +81,56 @@ public class EnemyController : MonoBehaviour {
 				}
 			}
 		}
+	}
+
+	private void NavAnimSetup()
+	{
+		// Create the parameters to pass to the helper function.
+		float speed;
+		float angle;
+		
+		// If the player is in sight...
+		if(isPlayerInSight)
+		{
+			// ... the enemy should stop...
+			speed = 0f;
+			
+			// ... and the angle to turn through is towards the player.
+			angle = FindAngle(transform.forward, playerGameObject.transform.position - transform.position, transform.up);
+		}
+		else
+		{
+			// Otherwise the speed is a projection of desired velocity on to the forward vector...
+			speed = Vector3.Project(agent.desiredVelocity, transform.forward).magnitude;
+			
+			// ... and the angle is the angle between forward and the desired velocity.
+			angle = FindAngle(transform.forward, agent.desiredVelocity, transform.up);
+			
+			// If the angle is within the deadZone...
+			if(Mathf.Abs(angle) < deadZone)
+			{
+				// ... set the direction to be along the desired direction and set the angle to be zero.
+				transform.LookAt(transform.position + agent.desiredVelocity);
+				angle = 0f;
+			}
+		}
+		
+		// Call the Setup function of the helper class with the given parameters.
+		animSetup.Setup(speed, angle);
+	}
+	
+	private float FindAngle(Vector3 fromVector, Vector3 toVector, Vector3 upVector)
+	{
+		if(toVector == Vector3.zero){
+			return 0f;
+		}
+
+		float angle = Vector3.Angle(fromVector, toVector);
+		Vector3 normal = Vector3.Cross(fromVector, toVector);
+		angle *= Mathf.Sign(Vector3.Dot(normal, upVector));
+		angle *= Mathf.Deg2Rad;
+		
+		return angle;
 	}
 
 	void OnTriggerStay (Collider other){
