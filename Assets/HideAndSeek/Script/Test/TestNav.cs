7c5c0b2383c458cf504b4e9c89cf1821401c7e64
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-24 13:55:40 +0900

    エネミーの経路探索の処理を一部修正

--- Assets/HideAndSeek/Script/Test/TestNav.cs
+++ Assets/HideAndSeek/Script/Test/TestNav.cs
@@ -16,32 +16,15 @@ public class TestNav : MonoBehaviour {
 	}
 
 	void Update(){
-		Debug.Log(string.Format("remaining = {0}   stoppingDistance = {1}", navAgent.remainingDistance, navAgent.stoppingDistance));
 		if(navAgent.remainingDistance < navAgent.stoppingDistance){
 			navAgent.SetDestination(target[pointNum].position);
 			float angle = FindAngle(transform.forward, target[pointNum].position-transform.position, transform.up);
 			AnimatorControl(speed, angle);
-		}
-	}
-
-	void OnTriggerEnter(Collider other){
-		if(other.tag=="Point"){
-			Debug.Log("check point!!");
 			int targetLength = target.Length-1;
-			if(pointNum>=targetLength){
-				pointNum--;
-			}else{
-				pointNum++;
-			}
-//			Debug.Log("check point!!");
-//			pointNum++;
-//			int targetLength = target.Length-1;
-//			if(pointNum>=targetLength){
-//				pointNum = 0;
-//			}
+			pointNum = pointNum>=targetLength ? pointNum-1 : pointNum+1; 
 		}
 	}
-	
+		
 	private float FindAngle(Vector3 fromVector, Vector3 toVector, Vector3 upVector){
 		if(toVector == Vector3.zero){
 			return 0f;
@@ -54,23 +37,7 @@ public class TestNav : MonoBehaviour {
 		
 		return angle;
 	}
-
-//	private bool FindGoalPoint(Transform targetTrans){
-//		bool isGoalPoint = false;
-//		float x1 = targetTrans.position.x - 5f;
-//		float x2 = targetTrans.position.x + 5f;
-//		float z1 = targetTrans.position.z - 5f;
-//		float z2 = targetTrans.position.z + 5f;
-//
-//		if(transform.position.x>x1 && transform.position.x<x2){
-//			if(transform.position.z>z1 && transform.position.z<z2){
-//				Debug.Log("chaeck point!!");
-//				isGoalPoint = true;
-//			}
-//		}
-//		return isGoalPoint;
-//	}
-
+	
 	private void AnimatorControl(float speed, float angle){
 		animator.SetFloat("Speed", speed);
 		animator.SetFloat("Direction", angle);
