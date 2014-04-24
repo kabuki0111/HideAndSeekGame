ba2c0764af5052f2085738da071c71955f35b301
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-24 13:41:51 +0900

    エネミーの挙動に関する不具合を修正（モック上では）

--- Assets/HideAndSeek/Script/Test/TestNav.cs
+++ Assets/HideAndSeek/Script/Test/TestNav.cs
@@ -8,6 +8,7 @@ public class TestNav : MonoBehaviour {
 	private Animator animator;
 	private float speed = 0.1f;
 	private int pointNum = 0;
+	private float timer = 0;
 
 	void Awake(){
 		navAgent = GetComponent<NavMeshAgent>();
@@ -15,21 +16,29 @@ public class TestNav : MonoBehaviour {
 	}
 
 	void Update(){
-		navAgent.SetDestination(target[pointNum].position);
-		float angle = FindAngle(transform.forward, target[pointNum].position-transform.position, transform.up);
-//		animator.SetFloat("Speed", speed);
-//		animator.SetFloat("Direction", angle);
-		AnimatorControl(speed, angle);
+		Debug.Log(string.Format("remaining = {0}   stoppingDistance = {1}", navAgent.remainingDistance, navAgent.stoppingDistance));
+		if(navAgent.remainingDistance < navAgent.stoppingDistance){
+			navAgent.SetDestination(target[pointNum].position);
+			float angle = FindAngle(transform.forward, target[pointNum].position-transform.position, transform.up);
+			AnimatorControl(speed, angle);
+		}
 	}
 
 	void OnTriggerEnter(Collider other){
 		if(other.tag=="Point"){
-			Debug.Log("hoge");
-			if(pointNum>=(target.Length-1)){
+			Debug.Log("check point!!");
+			int targetLength = target.Length-1;
+			if(pointNum>=targetLength){
 				pointNum--;
 			}else{
 				pointNum++;
 			}
+//			Debug.Log("check point!!");
+//			pointNum++;
+//			int targetLength = target.Length-1;
+//			if(pointNum>=targetLength){
+//				pointNum = 0;
+//			}
 		}
 	}
 	
@@ -46,21 +55,21 @@ public class TestNav : MonoBehaviour {
 		return angle;
 	}
 
-	private bool FindGoalPoint(Transform targetTrans){
-		bool isGoalPoint = false;
-		float x1 = targetTrans.position.x - 5f;
-		float x2 = targetTrans.position.x + 5f;
-		float z1 = targetTrans.position.z - 5f;
-		float z2 = targetTrans.position.z + 5f;
-
-		if(transform.position.x>x1 && transform.position.x<x2){
-			if(transform.position.z>z1 && transform.position.z<z2){
-				Debug.Log("chaeck point!!");
-				isGoalPoint = true;
-			}
-		}
-		return isGoalPoint;
-	}
+//	private bool FindGoalPoint(Transform targetTrans){
+//		bool isGoalPoint = false;
+//		float x1 = targetTrans.position.x - 5f;
+//		float x2 = targetTrans.position.x + 5f;
+//		float z1 = targetTrans.position.z - 5f;
+//		float z2 = targetTrans.position.z + 5f;
+//
+//		if(transform.position.x>x1 && transform.position.x<x2){
+//			if(transform.position.z>z1 && transform.position.z<z2){
+//				Debug.Log("chaeck point!!");
+//				isGoalPoint = true;
+//			}
+//		}
+//		return isGoalPoint;
+//	}
 
 	private void AnimatorControl(float speed, float angle){
 		animator.SetFloat("Speed", speed);
