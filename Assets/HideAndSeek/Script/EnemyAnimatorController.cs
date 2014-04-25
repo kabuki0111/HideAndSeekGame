e501c80cc3e08258f89767baea6e574b1287b80a
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-25 15:01:25 +0900

    Unityのメカニムに関連するファイルをコミット

---
+++ Assets/HideAndSeek/Script/EnemyAnimatorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimatorController : MonoBehaviour {
+	public int dyingState;
+	public int locomotionState;
+	public int deadBool;
+	public int speedFloat;
+	public int shotFloat;
+	public int angularSpeedFloat;
+
+	// Use this for initialization
+	void Awake(){
+		dyingState = Animator.StringToHash("Base Layer.Dying");
+		locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+		deadBool = Animator.StringToHash("Dead");
+		speedFloat = Animator.StringToHash("Speed");
+		shotFloat = Animator.StringToHash("Shot");
+		angularSpeedFloat = Animator.StringToHash("AngularSpeed");
+	}
+}
