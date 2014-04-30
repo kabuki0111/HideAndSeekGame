caba50ad478f5271256b0a0c7a5328867ef56356
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-30 16:47:50 +0900

    ドアの処理を作成

---
+++ Assets/HideAndSeek/Script/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	private Vector3 fromPosition;
+	private Vector3 toPosition;
+
+	void Awake(){
+		fromPosition = transform.position;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Door open!!");
+			iTween.MoveTo(gameObject, iTween.Hash("x", fromPosition.x+6f, "time", 2.5f, "looptype", iTween.LoopType.none));
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Door Close!!");
+			iTween.MoveTo(gameObject, iTween.Hash("x", fromPosition.x, "time", 2.5f, "looptype", iTween.LoopType.none));
+		}
+	}
+}
