892e89b2cca40de2ee44c6608100be6e82bf0d3d
Kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-07-01 19:39:10 +0900

    Save progress as communication events

--- Assets/HideAndSeek/Script/Helper/PathHelper.cs
+++ Assets/HideAndSeek/Script/Helper/PathHelper.cs
@@ -4,4 +4,10 @@ static public class PathHelper{
 	static public readonly string gameManagerPath = "GameController";
 	static public readonly string playerAttackColliderPath = "/Player/unitychan/Character1_Reference/Character1_Hips/Character1_LeftUpLeg/Character1_LeftLeg/Character1_LeftFoot";
 	static public readonly string hpSpritePath = "/UI Root/UIWidgetHitEffect";
+
+	//seelect window path
+	static public readonly string selectCharaLeftSpritePath = "/UI Root_Reset/SpaceChara_Left";
+	static public readonly string selectCharaRightSpritePath = "/UI Root_Reset/SpaceChara_Right";
+	static public readonly string selectNameLabelPath = "/UI Root_Reset/Label_Name";
+	static public readonly string selectWordLabelPath = "/UI Root_Reset/Label_ConvSent";
 }
---
+++ Assets/HideAndSeek/Script/Manager/SelectManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class SelectManager : MonoBehaviour {
+	private UISprite uiSpriteCharaLeft;	// hidari
+	private UISprite uiSpriteCharaRight; // migi
+	private UILabel charaNameLabel;	//
+	private UILabel charaWordLabel;	//
+
+	private void Awake(){
+		uiSpriteCharaLeft = GameObject.Find(PathHelper.selectCharaLeftSpritePath).GetComponent<UISprite>();
+		uiSpriteCharaRight = GameObject.Find(PathHelper.selectCharaRightSpritePath).GetComponent<UISprite>();
+		charaNameLabel = GameObject.Find(PathHelper.selectNameLabelPath).GetComponent<UILabel>();
+		charaWordLabel = GameObject.Find(PathHelper.selectWordLabelPath).GetComponent<UILabel>();
+		Debug.Log(string.Format("texture left -> {0}, texture right -> {1}, chara name -> {2}, chara word -> {3}",
+		                        uiSpriteCharaLeft, uiSpriteCharaRight, charaNameLabel, charaWordLabel));
+	}
+
+
+	private int testIndex = 0;
+	private void Start(){
+		List<string> wordList = WordListElement.FindEventCommunicationList("story00.txt");
+		StartEventUI(wordList, ref testIndex);
+		Debug.Log("testIndex  "+testIndex);
+	}
+
+	
+	private void StartEventUI(List<string> targetList, ref int targetIndex){
+
+		for(int i=targetIndex; i<targetList.Count; i++){
+			string[] fruit = targetList[i].Split(',');
+			string eventType = fruit[0];
+
+			if(eventType == "say"){return;}
+
+			switch(eventType){
+			case "start":
+				SetStartEvent();
+				break;
+			case "ui":
+				SetUI(fruit);
+				break;
+			case "bgm":
+				SetBgm(fruit[1]);
+				break;
+			case "end":
+				SetEventEnd();
+				break;
+			}
+
+			targetIndex++;
+		}
+	}
+
+	//start
+	private void SetStartEvent(){
+		this.gameObject.SetActive(true);
+	}
+
+	//ui
+	private void SetUI(string[] tartgetFruit){
+		switch(tartgetFruit[1]){
+		case "left":
+			uiSpriteCharaLeft.spriteName = FindSprite(tartgetFruit[2]);
+			break;
+		case "right":
+			uiSpriteCharaRight.spriteName = FindSprite(tartgetFruit[2]);
+			break;
+		}
+	}
+
+	private string FindSprite(string spriteName){
+		if(spriteName =="unity"){
+			return "Photo_Unitychan";
+		}else if(spriteName =="ser"){
+			return "Photo_Sergeant";
+		}else if(spriteName =="pres"){
+			return "Photo_President";
+		}
+		return "";
+	}
+
+	//bgm
+	private void SetBgm(string bgmName){
+		Debug.Log("set bgm ---->"+bgmName);
+	}
+
+	//end
+	private void SetEventEnd(){
+		Destroy(this.gameObject);
+	}
+}
